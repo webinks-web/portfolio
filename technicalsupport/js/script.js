90ae455b6c6c24aa5c9a3d1f9d5ebe4872e62f16
Abhinay Sunny Bitra <abhinay.tob@gmail.com>
2017-02-10 20:56:37 +0530

    added date and time

--- technicalsupport/js/script.js
+++ technicalsupport/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+  // BG changer
+
   $('.pink').click(function(){
     $('body').css('background','#A23E48');
   });
@@ -8,4 +11,39 @@ $(document).ready(function(){
   $('.yellow').click(function(){
     $('body').css('background','#FFD07B');
   });
+  $('.white').click(function(){
+    $('body').css('background','white');
+  });
+  $('.black').click(function(){
+    $('body').css('background','black');
+  });
+
+  // Date and Time
+  function updatetime(){
+    var now = new Date().toLocaleTimeString();
+    $('#time').html(now);
+  }
+
+  setInterval(updatetime,1000);
+  updatetime();
+  
+  function date(){
+    var today = new Date().toLocaleDateString();
+    $('#date').html(today);
+  }
+  date();
+
+  // var today = new Date();
+  // var dd = today.getDate();
+  // var mm = today.getMonth()+1; //January is 0!
+
+  // var yyyy = today.getFullYear();
+  // if(dd<10){
+  //     dd='0'+dd;
+  // } 
+  // if(mm<10){
+  //     mm='0'+mm;
+  // } 
+  // var today = dd+'/'+mm+'/'+yyyy;
+  // document.getElementById("DATE").value = today;
 });
